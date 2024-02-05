b5d239a667bd8441d049c3be5af37da3d099b7af
irfy2k321 <151876379+irfy2k321@users.noreply.github.com>
2024-02-05 23:45:58 +0500

    Time: 19 ms (90.75%) | Memory: 11.7 MB (33.26%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -1,14 +1,32 @@
 class Solution {
 public:
+#include <string>
 
-    int firstUniqChar(string s) {
-        vector<int> freq(26, 0);
-        for(auto ch : s){
-            freq[ch - 'a']++;
-        }
-        for(int i=0; i<s.size(); i++){
-            if(freq[s[i] - 'a'] == 1) return i;
-        }
-        return -1;
+int firstUniqChar(std::string s) {
+    int frequency[26] = {0};
+    int indexes[26];
+
+    for (int i = 0; i < 26; i++) {
+        indexes[i] = -1;
     }
+
+    for (int i = 0; i < s.size(); i++) {
+        char c = s[i];
+        frequency[c - 'a']++;
+        if (indexes[c - 'a'] == -1) {
+            indexes[c - 'a'] = i;
+        }
+    }
+
+    int firstIndex = -1;
+    for (int i = 0; i < 26; i++) {
+        if (frequency[i] == 1) {
+            if (firstIndex == -1 || indexes[i] < firstIndex) {
+                firstIndex = indexes[i];
+            }
+        }
+    }
+
+    return firstIndex;
+}
 };
