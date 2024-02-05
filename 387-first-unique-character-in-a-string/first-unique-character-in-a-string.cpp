45ea7f7595ed98de93ae49e65bd15285d34e5924
irfy2k321 <151876379+irfy2k321@users.noreply.github.com>
2024-02-05 23:24:09 +0500

    Time: 9 ms (99.72%) | Memory: 11.8 MB (27.34%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
-    #include <string>
-#include <limits>
+#include <string>
 
 int firstUniqChar(std::string s) {
     int frequency[26] = {0};
     int indexes[26];
 
     for (int i = 0; i < 26; i++) {
-        indexes[i] = s.size();
+        indexes[i] = -1;
     }
 
     for (int i = 0; i < s.size(); i++) {
         char c = s[i];
         frequency[c - 'a']++;
-        indexes[c - 'a'] = std::min(indexes[c - 'a'], i);
-    }
-
-    int firstIndex = s.size();
-    for (int i = 0; i < 26; i++) {
-        if (frequency[i] == 1) {
-            firstIndex = std::min(firstIndex, indexes[i]);
+        if (indexes[c - 'a'] == -1) {
+            indexes[c - 'a'] = i;
         }
     }
 
-    return (firstIndex == s.size()) ? -1 : firstIndex;
+    int firstIndex = -1;
+    for (int i = 0; i < 26; i++) {
+        if (frequency[i] == 1) {
+            if (firstIndex == -1 || indexes[i] < firstIndex) {
+                firstIndex = indexes[i];
+            }
+        }
+    }
+
+    return firstIndex;
 }
 };
