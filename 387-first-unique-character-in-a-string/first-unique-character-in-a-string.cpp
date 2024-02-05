328096b480f4ddb7471fe4c0b25874676f655d3d
irfy2k321 <151876379+irfy2k321@users.noreply.github.com>
2024-02-05 23:46:44 +0500

    Time: 24 ms (78.34%) | Memory: 11.7 MB (33.26%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -4,29 +4,17 @@ public:
 
 int firstUniqChar(std::string s) {
     int frequency[26] = {0};
-    int indexes[26];
 
-    for (int i = 0; i < 26; i++) {
-        indexes[i] = -1;
+    for (char c : s) {
+        frequency[c - 'a']++;
     }
 
     for (int i = 0; i < s.size(); i++) {
-        char c = s[i];
-        frequency[c - 'a']++;
-        if (indexes[c - 'a'] == -1) {
-            indexes[c - 'a'] = i;
+        if (frequency[s[i] - 'a'] == 1) {
+            return i;
         }
     }
 
-    int firstIndex = -1;
-    for (int i = 0; i < 26; i++) {
-        if (frequency[i] == 1) {
-            if (firstIndex == -1 || indexes[i] < firstIndex) {
-                firstIndex = indexes[i];
-            }
-        }
-    }
-
-    return firstIndex;
+    return -1;
 }
 };
