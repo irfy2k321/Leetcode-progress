124b55dfaa8b4daf32bc9dabe19953b6c21afd99
irfy2k321 <151876379+irfy2k321@users.noreply.github.com>
2024-02-09 00:58:18 +0500

    Time: 0 ms (100.00%) | Memory: 7.4 MB (90.90%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,18 +1,45 @@
-class Solution {
+class Solution 
+{  
+private:  
+    int is_square(int n)
+    {  
+        int sqrt_n = (int)(sqrt(n));  
+        return (sqrt_n*sqrt_n == n);  
+    }
+    
 public:
-    int numSquares(int n) {
-    int dp[n + 1];
-    for (int i = 0; i <= n; ++i) {
-        dp[i] = i;  // maximum is i because 1 is a perfect square
-    }
-    dp[0] = 0;
-    for (int i = 1; i <= n; ++i) {
-        for (int j = 1; j*j <= i; ++j) {
-            if (dp[i - j*j] + 1 < dp[i]) {
-                dp[i] = dp[i - j*j] + 1;
-            }
+    // Based on Lagrange's Four Square theorem, there 
+    // are only 4 possible results: 1, 2, 3, 4.
+    int numSquares(int n) 
+    {  
+        // If n is a perfect square, return 1.
+        if(is_square(n)) 
+        {
+            return 1;  
         }
-    }
-    return dp[n];
-}
-};
+        
+        // The result is 4 if and only if n can be written in the 
+        // form of 4^k*(8*m + 7). Please refer to 
+        // Legendre's three-square theorem.
+        while ((n & 3) == 0) // n%4 == 0  
+        {
+            n >>= 2;  
+        }
+        if ((n & 7) == 7) // n%8 == 7
+        {
+            return 4;
+        }
+        
+        // Check whether 2 is the result.
+        int sqrt_n = (int)(sqrt(n)); 
+        for(int i = 1; i <= sqrt_n; i++)
+        {  
+            if (is_square(n - i*i)) 
+            {
+                return 2;  
+            }
+        }  
+        
+        return 3;  
+    }  
+}; 
